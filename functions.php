5ece81ac99ba9f27e7de876be4102d952855745d
Shyanne Manuel <manuelshyanne@gmail.com>
2025-03-25 13:27:36 -0700

    created functions.php file and added enqueu for style.css

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function schoolsite_enqueues() {
+	// Load style.css on the front-end
+	// Parameters: Unique handle, Source, Dependencies, Version number, Media
+	wp_enqueue_style( 
+		'schoolsite-style',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get( 'Version' ),
+		'all'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'schoolsite_enqueues' );
