c3b70f68abb7e1b3959e17e947a61f86a8cfbd47
Shyanne Manuel <manuelshyanne@gmail.com>
2025-03-25 13:55:06 -0700

    added normalize.css

--- functions.php
+++ functions.php
@@ -10,5 +10,22 @@ function schoolsite_enqueues() {
 		wp_get_theme()->get( 'Version' ),
 		'all'
 	);
+
+    // Load normalize.css on the front-end
+    wp_enqueue_style( 
+        'schoolsite-normalize', 
+        get_theme_file_uri( 'assets/css/normalize.css'), 
+        array(), 
+        '12.1.0'
+    );
 }
 add_action( 'wp_enqueue_scripts', 'schoolsite_enqueues' );
+
+
+function schoolsite_setup() {
+    // Adds style to the editor
+    add_editor_style(get_stylesheet_uri());
+}
+
+add_action( 'after_setup_theme', 'schoolsite_setup' );
+
