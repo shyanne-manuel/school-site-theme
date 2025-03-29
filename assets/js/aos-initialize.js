ad33fe7e96cb20be9fc8a87bd983793f31d8dfca
Shyanne Manuel <manuelshyanne@gmail.com>
2025-03-29 10:36:41 -0700

    Modified aos-initialize.js

--- assets/js/aos-initialize.js
+++ assets/js/aos-initialize.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     AOS.init({
         duration: 400, // Duration of animations
         easing: 'ease-in-out', // Easing function for animations
-        once: true,
     });
   
 });
