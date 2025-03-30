b42d44940014310161d1d4786416f40cac212e37
Shyanne Manuel <manuelshyanne@gmail.com>
2025-03-29 21:37:43 -0700

    Added nonsynced pattern featured-event.php

---
+++ patterns/featured-event.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Featured Event 
+ * Slug: school-site-theme/featured-event
+ * Categories: media, text
+ */
+?>
+
+<!-- wp:school-site-blocks/animate-on-scroll -->
+<div class="wp-block-school-site-blocks-animate-on-scroll" data-aos="fade-up"><!-- wp:media-text {"mediaId":21,"mediaLink":"http://localhost/cms/school/pug-working/","mediaType":"image"} -->
+<div class="wp-block-media-text is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="http://localhost/cms/school/wp-content/uploads/2025/03/pug-working-1024x683.jpg" alt="A pug working" class="wp-image-21 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:school-site-blocks/animate-on-scroll {"animation":"zoom-in"} -->
+<div class="wp-block-school-site-blocks-animate-on-scroll" data-aos="zoom-in"><!-- wp:heading -->
+<h2 class="wp-block-heading">News/Event Title</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"placeholder":"Content…"} -->
+<p>Info or description</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:school-site-blocks/animate-on-scroll --></div></div>
+<!-- /wp:media-text --></div>
+<!-- /wp:school-site-blocks/animate-on-scroll -->
