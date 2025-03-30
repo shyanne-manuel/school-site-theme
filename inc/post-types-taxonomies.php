6416a98dc8ed012f106472c640910eec37ff6ac3
William Lin <withawill_@outlook.com>
2025-03-29 20:16:17 -0700

    remove Testimonial CPT

--- inc/post-types-taxonomies.php
+++ inc/post-types-taxonomies.php
@@ -136,47 +136,6 @@ $args = array(
 );
 register_post_type( 'fwd-staff', $args );
 
-
-// Testimonial Custom Post Type
-$labels = array(
-    'name'                  => _x( 'Testimonials', 'post type general name', 'mindset-theme' ),
-    'singular_name'         => _x( 'Testimonial', 'post type singular name', 'mindset-theme' ),
-    'menu_name'             => _x( 'Testimonials', 'admin menu', 'mindset-theme' ),
-    'add_new'               => _x( 'Add New', 'testimonial', 'mindset-theme' ),
-    'add_new_item'          => __( 'Add New Testimonial', 'mindset-theme' ),
-    'new_item'              => __( 'New Testimonial', 'mindset-theme' ),
-    'edit_item'             => __( 'Edit Testimonial', 'mindset-theme' ),
-    'view_item'             => __( 'View Testimonial', 'mindset-theme'  ),
-    'all_items'             => __( 'All Testimonials', 'mindset-theme' ),
-    'search_items'          => __( 'Search Testimonials', 'mindset-theme' ),
-    'parent_item_colon'     => __( 'Parent Testimonials:', 'mindset-theme' ),
-    'not_found'             => __( 'No testimonials found.', 'mindset-theme' ),
-    'not_found_in_trash'    => __( 'No testimonials found in Trash.', 'mindset-theme' ),
-    'item_link'             => __( 'Testimonial link.', 'mindset-theme' ),
-    'item_link_description' => __( 'A link to a testimonial.', 'mindset-theme' ),
-);
-
-$args = array(
-    'labels'             => $labels,
-    'public'             => true,
-    'publicly_queryable' => true,
-    'show_ui'            => true,
-    'show_in_menu'       => true,
-    'show_in_rest'       => true,
-    'query_var'          => true,
-    'rewrite'            => array( 'slug' => 'testimonials' ),
-    'capability_type'    => 'post',
-    'has_archive'        => false,
-    'hierarchical'       => false,
-    'menu_position'      => 7,
-    'menu_icon'          => 'dashicons-heart',
-    'supports'           => array( 'title', 'editor' ),
-    'template'           => array( array( 'core/pullquote' ) ),
-    'template_lock'      => 'all'
-);
-
-register_post_type( 'fwd-testimonial', $args );
-
 }
 add_action( 'init', 'schoolsite_custom_post_types' );
 
